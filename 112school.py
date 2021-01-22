000b1960c7d490a52e82813c69d2b58633588948
Borisonix <b.sharapov.nvk@gmail.com>
2021-01-22 14:30:35 +0700

    Увеличена высота зачистки до 150

--- 112school.py
+++ 112school.py
@@ -6,26 +6,18 @@ from Build import *
 
 # mc = Minecraft.create()
 
-
-
-''' для первичного позиционирования игрока в новом мире
-posMy = posMAIN.clone()
-posMy.y += 5
-mc.player.setTilePos(posMy)
-'''
-
 posZACHISTKA = posMAIN.clone()
 posZACHISTKA.x += 300
-posZACHISTKA.y += 50
+posZACHISTKA.y += 150
 posZACHISTKA.z += 400
-mc.setBlocks(posMAIN, posZACHISTKA, AIR)
+mc.setBlocks(posMAIN, posZACHISTKA, AIR)    # зачистка территории
 
 posZACHISTKA.y = posMAIN.y
-mc.setBlocks(posMAIN, posZACHISTKA, STONE)
+mc.setBlocks(posMAIN, posZACHISTKA, STONE)  # каменная площадка
 
 posMAIN.x += 20
 posMAIN.y += 1
 posMAIN.z += 70
 
-school112()         # постройка школы
-# walls4()
+# school112()         # постройка школы
+walls4()
